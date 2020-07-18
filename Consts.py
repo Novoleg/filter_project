223d691ffafbc75e7f47b22e7cbe9d0b2603580c
Novoleg <65327076+Novoleg@users.noreply.github.com>
2020-07-18 22:07:06 +0700

    Добавил файл Consts

---
+++ Consts.py
@@ -0,0 +1,18 @@
+class Consts:
+    def __init__(self, w, dwp, big_w, dt):
+        self.w = w
+        self.dwp = dwp
+        self.big_w = big_w
+        self.dt = dt
+
+    def result_wp(self):
+        return self.w - self.dwp / 2
+
+    def result_big_wp(self):
+        return self.big_w + self.dwp / 2
+
+    def result_wzw(self):
+        return self.result_wp() * self.dt
+
+    def result_big_wzv(self):
+        return self.result_big_wp() * self.dt
